43b98c7a6fac699e41aca36c090418100761acd7
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-03-28 14:02:20 -0600

    MVP

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+  
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBAction func onAddButtonTapped(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "What is Your Name?", message: nil, preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+        
+        let ok = UIAlertAction(title: "Ok", style: .default) {action in print("OK Pressed")
+        }
+        alert.addAction(ok)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancel)
+    }
+    
 
 }
 
